719086152c5681000279db8a6ca313b4030b0736
just-bm <tbalancemurugan@gmail.com>
2025-12-29 19:47:31 +0530

    done something

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://streamer-delta-six.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
